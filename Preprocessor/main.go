c2425a73d503c0c7bf839fc244544b32acae1702
MichaelBehr <mikeyogibear@gmail.com>
2020-07-09 14:31:56 -0400

    Implemented the main function to run the parser

---
+++ Preprocessor/main.go
@@ -0,0 +1,63 @@
+package Preprocessor
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"runtime/debug"
+	"strings"
+)
+
+func main() {
+	// defer profile.Start().Stop()
+	debug.SetGCPercent(300)
+	var (
+		verbose bool
+		count   bool
+		help    bool
+	)
+	flag.BoolVar(&verbose, "verbose", false, "sets verbose mode on")
+	flag.BoolVar(&count, "count", false, "rather than solving the problem, counts the number of models it accepts")
+	flag.BoolVar(&help, "help", false, "displays help")
+	flag.Parse()
+	if !help && len(flag.Args()) != 1 {
+		fmt.Printf("This is GoPreProcessor. Functions taken from Gophersat. Modifications/additions by Michael Behr.\n")
+		fmt.Fprintf(os.Stderr, "Syntax : %s [options] (file.cnf|file.wcnf|file.bf|file.opb)\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	if help {
+		fmt.Printf("This is GoPreProcessor version 1.0, a SAT pre-processor by Michael Behr and Jared Lenos.\n")
+		fmt.Printf("Syntax : %s [options] (file.cnf|file.wcnf|file.bf|file.opb)\n", os.Args[0])
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	path := flag.Args()[0]
+	fmt.Printf("c solving %s\n", path)
+	if strings.HasSuffix(path, ".cnf") {
+		if pb, err := parse(flag.Args()[0]); err != nil {
+			fmt.Fprintf(os.Stderr, "could not parse problem: %v\n", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("CNF FORMULA:\n\n",pb.CNF())
+		}
+	} else{
+		fmt.Fprintf(os.Stderr, "Could not parse problem. Make sure it is in CNF form.")
+	}
+
+}
+func parse(path string) (pb *Problem, err error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("could not open %q: %v", path, err)
+	}
+	defer f.Close()
+	if strings.HasSuffix(path, ".cnf") {
+		pb, err := ParseCNF(f)
+		if err != nil {
+			return nil, fmt.Errorf("could not parse DIMACS file %q: %v", path, err)
+		}
+		return pb,nil
+	}
+	return nil, fmt.Errorf("invalid file format for %q", path)
+}
